0a2062b1eba94ab1ad69e2ac2c686c193cbfa1fb
Wojciech Jablonski <wojciech-jablonski@outlook.com>
2018-11-07 20:39:34 +0100

    Basic graphic elements.
    Card model, deck of card model, flipping card animation test, getting cards from deck functionality.

---
+++ Assets/CardStack.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+using System;
+
+public class CardStack : MonoBehaviour
+{
+    List<int> cards;
+    public bool HasCards { get { return cards != null && cards.Count > 0; } }
+    public bool isGameDeck;
+    public int CardCount { get { return cards == null ? 0 : cards.Count; } }
+
+    public event CardRemovedEventHandler CardRemoved;
+
+    public void CreateDeck()
+    {
+        cards.Clear();
+
+        for (int i = 0; i < 52; i++)
+        {
+            cards.Add(i);
+        }
+
+        var shuffledDeck = new List<int>();
+        var rand = new System.Random();
+
+        while (cards.Count > 0)
+        {
+            int index = rand.Next(0, cards.Count - 1);
+            int cardNumber = cards[index];
+            shuffledDeck.Add(cardNumber);
+            cards.RemoveAt(index);
+        }
+
+        cards = shuffledDeck;
+    }
+
+	void Start ()
+    {
+        cards = new List<int>();
+        if (isGameDeck)
+        {
+            CreateDeck();
+        }
+	}
+
+    public IEnumerable<int> GetCards()
+    {
+        foreach (int i in cards)
+        {
+            yield return i;
+        }
+    }
+
+    public int Pop()
+    {
+        int temp = cards[CardCount - 1];
+        cards.RemoveAt(CardCount - 1);
+
+        if (CardRemoved != null)
+        {
+            CardRemoved(this, new CardRemovedEventArgs(temp));
+        }
+
+        return temp;
+    }
+
+    public void Push(int card)
+    {
+        cards.Add(card);
+    }
+}
---
+++ Assets/CardStackView.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CardStack))]
+public class CardStackView : MonoBehaviour
+{
+    CardStack deck;
+    Dictionary<int, GameObject> fetchedCards;
+    int lastCount;
+
+    public Vector3 start;
+    public GameObject cardPrefab;
+    public float cardOffset;
+    public bool faceUp = false;
+
+	void Start()
+    {
+        fetchedCards = new Dictionary<int, GameObject>();
+        deck = GetComponent<CardStack>();
+        ShowCards();
+        lastCount = deck.CardCount;
+
+        deck.CardRemoved += Deck_CardRemoved;
+    }
+
+    private void Deck_CardRemoved(object sender, CardRemovedEventArgs e)
+    {
+        if (fetchedCards.ContainsKey(e.CardIndex))
+        {
+            Destroy(fetchedCards[e.CardIndex]);
+            fetchedCards.Remove(e.CardIndex);
+        }
+    }
+
+    void Update()
+    {
+        if (lastCount != deck.CardCount)
+        {
+            lastCount = deck.CardCount;
+            ShowCards();
+        }
+    }
+
+    void ShowCards()
+    {
+        int cardCount = 0;
+
+        if (!deck.HasCards)
+            return;
+
+        foreach (int i in deck.GetCards())
+        {
+            float co = cardOffset * cardCount;
+
+            Vector3 temp = start + new Vector3(co, 0f);
+            AddCard(temp, i, cardCount);
+
+            cardCount++;
+        }
+    }
+
+    void AddCard(Vector3 position, int cardIndex, int positionalIndex)
+    {
+        if (fetchedCards.ContainsKey(cardIndex))
+            return;
+
+        GameObject cardCopy = (GameObject)Instantiate(cardPrefab);
+        cardCopy.transform.position = position;
+
+        CardModel cardModel = cardCopy.GetComponent<CardModel>();
+        cardModel.cardIndex = cardIndex;
+        cardModel.ToggleFace(faceUp);
+
+        SpriteRenderer spriteRenderer = cardCopy.GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingOrder = positionalIndex;
+
+        fetchedCards.Add(cardIndex, cardCopy);
+    }
+}
